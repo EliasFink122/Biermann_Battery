0c859c092f6c88d28549d258588706831a0e07b5
Elias Fink <elias.fink@gmx.de>
2024-07-16 15:22:35 +0100

    Beam propagation

--- BB_RCF.py
+++ BB_RCF.py
@@ -7,8 +7,9 @@ Integrate magnetic field to find proton beam path.
 """
 import numpy as np
 from BB_Simple import beam, density, biermann_field
-from scipy.constants import elementary_charge as e
+from scipy.constants import k, elementary_charge as e
 from scipy.constants import proton_mass as m_p
+import matplotlib.pyplot as plt
 
 def maxwell(num = 1, temp = 1e7) -> np.ndarray:
     '''
@@ -24,13 +25,13 @@ def maxwell(num = 1, temp = 1e7) -> np.ndarray:
     vx = np.random.normal(size=num)
     vy = np.random.normal(size=num)
     vz = np.random.normal(size=num)
-    return np.sqrt(vx*vx + vy*vy + vz*vz)/m_p*temp
+    return np.sqrt((vx*vx + vy*vy + vz*vz)*(temp/e*k/m_p))
 
 class Proton():
     '''
     Proton base class
     '''
-    def __init__(self, pos = [0, 0, 10], vel = [0, 0, 1]):
+    def __init__(self, pos = [0., 0., 10.], vel = [0., 0., -1.]):
         self.__pos = np.array(pos)
         self.__vel = np.array(vel)
     def pos(self) -> np.ndarray:
@@ -65,14 +66,15 @@ class ProtonBeam():
     AMP = 10
     SPEC_AMP = 1
     WIDTH = 10
-    TIME_INCREMEMT = 1e-2
+    TIME_INCREMEMT = 1e-10
 
     def __init__(self, n_protons = 100, temperature = 10):
         self.__protons: list[Proton] = []
         temperature *= 1e6
         for _ in range(n_protons):
-            v_z = maxwell(temp = temperature)
-            self.__protons.append(Proton(vel = [0, 0, v_z]))
+            v_z = maxwell(temp = temperature)[0]
+            self.__protons.append(Proton(vel = [0, 0, -v_z]))
+            print(v_z)
     def protons(self) -> list:
         '''
         Proton array
@@ -111,3 +113,27 @@ class ProtonBeam():
         '''
         return beam(xyz, amp = ProtonBeam.AMP, spec_amp = ProtonBeam.SPEC_AMP,
                     width = ProtonBeam.WIDTH)
+    def send_beam(self, max_iter = 1e6) -> np.ndarray:
+        '''
+        Send beam through magnetic field
+
+        Returns:
+            final positions of protons
+        '''
+        positions = []
+        for _ in range(int(max_iter)):
+            self.propagate()
+            for i, proton in enumerate(self.__protons):
+                #print(proton.pos()[2])
+                if proton.pos()[2] <= 0:
+                    positions.append(proton.pos()[:2])
+                    self.__protons.pop(i)
+            if len(self.__protons) == 0:
+                break
+        return np.array(positions)
+
+if __name__ == "__main__":
+    sample_beam = ProtonBeam(2, 10)
+    position_arr = sample_beam.send_beam()
+    plt.hist2d(position_arr[:, 0], position_arr[:, 1], bins = 10)
+    plt.plot()
