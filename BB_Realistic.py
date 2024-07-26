80dea4c1aca837c565346a87310aa34170f574f5
Elias Fink <elias.fink@gmx.de>
2024-07-26 13:00:44 +0100

    More customisation

--- BB_Realistic.py
+++ BB_Realistic.py
@@ -33,13 +33,17 @@ def beam(amp, width, mod_amp, mod_freq, num) -> np.ndarray:
         value of modulation
     '''
     xs = np.linspace(-width*1.5, width*1.5, num)
-    xys = np.zeros((len(xs), len(xs), 2))
+    xyz = np.zeros((len(xs), len(xs), len(xs), 3))
+    phase = np.random.rand(num, num)*2*np.pi
+    phase_xyz = np.zeros(np.shape(xyz)[:3])
     for i, x in enumerate(xs):
         for j, y in enumerate(xs):
-            xys[i, j] = [x, y]
-    phase = np.random.rand(num, num)*2*np.pi
-    modulation = np.exp(mod_amp * np.sin(mod_freq*np.linalg.norm(xys, axis = 2))**2 * np.exp(1j*phase))
-    ideal_beam = amp*np.exp(-((np.linalg.norm(xys, axis = 2)**2)/(2*width**2))**5)
+            for k, z in enumerate(xs):
+                xyz[i, j, k] = [x, y, z]
+                phase_xyz[i, j, k] = phase[i, j]
+    exp = mod_amp * np.sin(mod_freq*(np.sqrt(xyz[:, :, :, 0]**2 + xyz[:, :, :, 1]**2)))**2
+    modulation = np.exp(exp) * np.exp(1j*phase_xyz)
+    ideal_beam = amp*np.exp(-((xyz[:, :, :, 0]**2 + xyz[:, :, :, 1]**2)/(2*width**2))**5)
     return np.abs(ideal_beam * modulation)
 
 def density(rho0, decay_length, num) -> np.ndarray:
@@ -58,7 +62,12 @@ def density(rho0, decay_length, num) -> np.ndarray:
     density_arr = np.zeros((len(zs)))
     for i, z_pos in enumerate(zs):
         density_arr[i] = rho0 * np.exp(-z_pos/decay_length)
-    return density_arr
+    density_xyz = np.zeros((num, num, num))
+    for i, x in enumerate(zs):
+        for j, y in enumerate(zs):
+            for k, z in enumerate(zs):
+                density_xyz[i, j, k] = density_arr[k]
+    return density_xyz
 
 def grad(arr, width) -> np.array:
     '''
@@ -75,18 +84,13 @@ def grad(arr, width) -> np.array:
 
     gradient_12d = np.array(np.gradient(arr, spacing))
 
-    if len(np.shape(arr)) == 1:
-        gradient = np.zeros((len(gradient_12d), len(gradient_12d), len(gradient_12d), 3))
-        for i, row1 in enumerate(gradient):
-            for j, row2 in enumerate(row1):
-                for k, _ in enumerate(row2):
-                    gradient[i, j, k] = np.array([0, 0, gradient_12d[k]])
-    elif len(np.shape(arr)) == 2:
-        gradient = np.zeros((len(gradient_12d[0]), len(gradient_12d[0]), len(gradient_12d[0]), 3))
-        for i, row1 in enumerate(gradient):
-            for j, row2 in enumerate(row1):
-                for k, _ in enumerate(row2):
-                    gradient[i, j, k] = np.array([gradient_12d[0, i, j], gradient_12d[1, i, j], 0])
+    gradient = np.zeros((len(gradient_12d[0]), len(gradient_12d[0]), len(gradient_12d[0]), 3))
+    for i, row1 in enumerate(gradient):
+        for j, row2 in enumerate(row1):
+            for k, _ in enumerate(row2):
+                gradient[i, j, k] = np.array([gradient_12d[0, i, j, k],
+                                                gradient_12d[1, i, j, k],
+                                                gradient_12d[2, i, j, k]])
 
     return gradient
 
@@ -132,7 +136,7 @@ if __name__ == "__main__":
     ax.quiver(x, y, z, bf[:, :, :, 1], bf[:, :, :, 0], bf[:, :, :, 2], length=0.00001,
               linewidth = 2, arrow_length_ratio = 0.3)
     x, y = np.meshgrid(x_arr, x_arr)
-    ax.plot_surface(x, y, mod_beam, cmap = "Oranges")
+    ax.plot_surface(x, y, mod_beam[:, :, 0], cmap = "Oranges")
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.set_zlabel("z")
