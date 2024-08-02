ab2e7ca85b03a3d4d7ffe4228c7a8531e51f550b
Elias Fink <elias.fink@gmx.de>
2024-08-02 09:49:18 +0100

    Restructuring

---
+++ dynamic/BB_ReadIn.py
@@ -0,0 +1,43 @@
+"""
+Created on Thu Aug 01 2024
+
+@author: Elias Fink (elias.fink22@imperial.ac.uk)
+
+Read in proton position files
+
+Methods:
+    read_in:
+        read in text results file
+    plot_data:
+        plot all data
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read_in(path = "results_1e2.0_even.txt"):
+    '''
+    Read in txt file
+
+    Args:
+        path: full path to file
+
+    Returns:
+        data array
+    '''
+    data = np.loadtxt(path)
+    return data
+
+def plot_data(data):
+    '''
+    Plot text file data on histogram
+    '''
+    plt.figure()
+    plt.title("Simulated RCF")
+    plt.hist2d(data[:, 0]*1000, data[:, 1]*1000, range = [[-200, 200], [-200, 200]], bins = 500)
+    plt.xlabel("x [mm]")
+    plt.ylabel("y [mm]")
+    plt.show()
+
+if __name__ == "__main__":
+    positions = read_in()
+    plot_data(positions)
